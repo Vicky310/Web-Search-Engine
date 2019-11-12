b0cf5a395dec5cb245c6c3481b20d3595da8ed94
Arvind Narayan <varvind7@gmail.com>
2019-11-12 00:10:57 -0500

    FileNotFound error in servlet

--- src/searchEngine/HandleSearch.java
+++ src/searchEngine/HandleSearch.java
@@ -1,6 +1,13 @@
 package searchEngine;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,6 +35,29 @@ public class HandleSearch extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println(request.getParameter("searchBox"));
+		String realContextPath = getServletContext().getRealPath("");
+//		System.out.println("REAL CONTEXT PATH: " + realContextPath);
+		try {
+			
+		
+		//FileInputStream file = (FileInputStream) getServletContext().getResourceAsStream("urldataset.txt");
+		BufferedReader file = new BufferedReader(new FileReader("D:\\Masters\\Advanced Computing Concepts\\Project\\Web-Search-Engine\\urldataset.txt"));
+		String url = file.readLine();
+		String content = null;
+		System.out.println("read File");
+		URLConnection connection = null;
+		
+		  connection =  new URL(url).openConnection();
+		  Scanner scanner = new Scanner(connection.getInputStream());
+		  scanner.useDelimiter("\\Z");
+		  content = scanner.next();
+		  scanner.close();
+		  System.out.println(content);
+		}catch ( Exception ex ) {
+		    //ex.printStackTrace();
+			System.out.print(ex);
+		}
+		
 	}
 
 }
