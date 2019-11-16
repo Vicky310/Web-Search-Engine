08baee2c8c7cf401d55e0cbc29635c0fdb2ed0cf
Vigneshwaran <balasubv@uwindsor.ca>
2019-11-15 23:50:09 -0500

    Jsoup working code

--- src/searchEngine/HandleSearch.java
+++ src/searchEngine/HandleSearch.java
@@ -1,16 +1,21 @@
 package searchEngine;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Scanner;
+import java.net.URL;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -18,6 +23,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 /**
  * Servlet implementation class HandleSearch
@@ -48,13 +56,18 @@ public class HandleSearch extends HttpServlet {
 		ServletContext cntxt = getServletContext();
 		String fName = "/WEB-INF/urlDataset.txt";
 		InputStream ins = cntxt.getResourceAsStream(fName);
+		String content = "";
 		if (ins == null) {
 			System.out.println("Not found");
 		} else {
 			BufferedReader br = new BufferedReader((new InputStreamReader(ins)));
 			String word;
 			while ((word = br.readLine()) != null) {
-				pw.println(word + "<br>");
+				System.out.println(word);
+				System.out.println("read File");
+				Document doc = Jsoup.connect(word).get();
+				System.out.println(doc.text());
+				break;
 			}
 		}
 	}
